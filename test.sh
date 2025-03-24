491de713debcc385f56af4bdf8f466283c5be471
YomiyuShambel21 <yamibkshambel@gmail.com>
2025-03-24 11:52:40 +0300

    test

--- test.sh
+++ test.sh
@@ -1,19 +1,22 @@
-# Check if we can connect to the MongoDB database
-MONGO_CONTAINER_NAME='fullstack-todo-list-mongo-1'
-echo "Checking the MongoDB Connection at localhost:27017..."
+#!/bin/bash
 
-# Ensure that the MongoDB container is running
-if ! docker ps -q -f name=$MONGO_CONTAINER_NAME; then
-  print_result 1 "Oops! The MongoDB container is not running."
-  exit 1
+# Checking if frontend is running
+if curl -s http://localhost > /dev/null; then
+  echo "Frontend is running."
+else
+  echo "Frontend is not running."
 fi
 
-# Try executing a command to get some stats from the MongoDB server
-docker exec -i $MONGO_CONTAINER_NAME mongosh --eval 'db.stats()' > db_test_output.txt 2>&1
-
-# Instead of checking for ok: 1, just confirm db.stats() runs without error
-if grep -q '"ok": 1' db_test_output.txt; then
-  print_result 0 "Successfully connected to MongoDB!"
+# Checking if backend is running
+if curl -s http://localhost:5000/api/todos > /dev/null; then
+  echo "Backend is running."
 else
-  print_result 1 "Failed to connect to MongoDB, but the stats command was executed."
+  echo "Backend is not running."
+fi
+
+# Checking if MongoDB is running
+if docker exec -it f2f4a391416a mongosh --eval "show dbs" > /dev/null; then
+  echo "MongoDB is running."
+else
+  echo "MongoDB is not running."
 fi
